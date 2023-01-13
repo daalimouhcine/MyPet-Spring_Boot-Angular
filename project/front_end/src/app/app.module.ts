9893f3f42b8e004832b3ce71763ada3156654a1b
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-13 16:18:27 +0100

    build(project): complete security in back-end

--- project/front_end/src/app/app.module.ts
+++ project/front_end/src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { AddOfferModalComponent } from './components/add-offer-modal/add-offer-modal.component';
+import { AddPostModalComponent } from './components/add-post-modal/add-post-modal.component';
+import { PostComponent } from './components/post/post.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddOfferModalComponent,
+    AddPostModalComponent,
+    PostComponent,
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
